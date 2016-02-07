6904011781a7ff023cda25d9078732a083e2f51a
Roman Gaechter <gaechterroman@gmail.com>
2016-02-07 19:35:35 +0100

    working

--- plot1.R
+++ plot1.R
@@ -1,1 +1,45 @@
 # code for first plot
+
+# URL of data source
+dataurl <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+
+# download and store file
+download.file(url=dataurl,
+              destfile = "./data/household_power_consumption.zip")
+dateDownloaded <- date()
+
+# unzip file
+unzipped <- unzip(zipfile = "./data/household_power_consumption.zip",
+                  exdir = "data")
+
+# read in data
+data <- read.csv(file = "./data/household_power_consumption.txt",
+                 sep = ";")
+
+# reformat data
+data$Date <- as.Date(strptime(data$Date, format = "%d/%m/%Y"))
+data$Global_active_power <- as.numeric(levels(data$Global_active_power))[data$Global_active_power]
+
+# restrict data
+dataToBeUsed <- subset(x = data,
+                       (data$Date >= "2007-02-01" & data$Date <= "2007-02-02"))
+
+# generate plot
+hist(x = dataToBeUsed$Global_active_power,
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency",
+     col = "red",
+     xlim = c(0, 6),
+     ylim = c(0, 1200),
+     axes = FALSE)
+axis(side = 1,
+     at = seq(0, 6, 2),
+     labels = seq(0, 6, 2))
+axis(side = 2,
+     at = seq(0, 1200, 200),
+     labels = seq(0, 1200, 200))
+
+# copy plot into PNG
+dev.copy(png, file = "plot1.png")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,55 @@
+# code for first plot
+
+# URL of data source
+dataurl <- "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
+
+# download and store file
+download.file(url=dataurl,
+              destfile = "./data/household_power_consumption.zip")
+dateDownloaded <- date()
+
+# unzip file
+unzipped <- unzip(zipfile = "./data/household_power_consumption.zip",
+                  exdir = "data")
+
+# read in data
+data <- read.csv(file = "./data/household_power_consumption.txt",
+                 sep = ";")
+
+# reformat data
+# data$Date <- as.Date(strptime(data$Date, format = "%d/%m/%Y"))
+# data$Time <- as.numeric(levels(data$Time))[data$Time]
+data$Global_active_power <- as.numeric(levels(data$Global_active_power))[data$Global_active_power]
+data$Global_reactive_power <- as.numeric(levels(data$Global_reactive_power))[data$Global_reactive_power]
+data$Voltage <- as.numeric(levels(data$Voltage))[data$Voltage]
+data$Sub_metering_1 <- as.numeric(levels(data$Sub_metering_1))[data$Sub_metering_1]
+data$Sub_metering_2 <- as.numeric(levels(data$Sub_metering_2))[data$Sub_metering_2]
+data$Sub_metering_3 <- as.numeric(levels(data$Sub_metering_3))[data$Sub_metering_3]
+
+DateTime <- as.POSIXlt(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+dataToBeUsed <- cbind(DateTime, data[, 3:9])
+
+# restrict data
+# dataToBeUsed <- subset(x = data,
+#                        (data$Date >= "2007-02-01" & data$Date <= "2007-02-02"))
+dataToBeUsed <- subset(x = dataToBeUsed,
+                       (dataToBeUsed$DateTime >= "2007-02-01 00:00:00 CET" & dataToBeUsed$DateTime <= "2007-02-02 23:59:59 CET"))
+
+# generate plot
+plot(x = dataToBeUsed$DateTime,
+     y = dataToBeUsed$Sub_metering_1,
+     ylab = "Energy sub metering",
+     xlab = "",
+     axes = TRUE,
+     type = "n")
+lines(dataToBeUsed$Sub_metering_1 ~ dataToBeUsed$DateTime, col = "black")
+lines(dataToBeUsed$Sub_metering_2 ~ dataToBeUsed$DateTime, col = "red")
+lines(dataToBeUsed$Sub_metering_3 ~ dataToBeUsed$DateTime, col = "blue")
+legend("topright",
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"),
+       lty = "solid", lwd = 1)
+
+# copy plot into PNG
+dev.copy(png, file = "plot3.png")
+dev.off()
